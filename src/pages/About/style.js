b0cbfd6582af26485485b4abf0a6f700eb7f77dc
miqueiasbelfort <miqueiasbelfort8323@gmail.com>
2022-03-03 14:14:35 -0300

    first commit

--- src/pages/About/style.js
+++ src/pages/About/style.js
@@ -50,7 +50,7 @@ export const Tecnologis = styled.div`
     }
     @media (max-width: 768px){
         width: 100%;
-        height: 400px;
+        height: 250px;
         justify-content: center;
         padding: 20px 0;
         .boxTec{
